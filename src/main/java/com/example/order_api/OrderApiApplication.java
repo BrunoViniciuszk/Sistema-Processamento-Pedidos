41d42cf2a1a912fcc0e532a232163f7cb2174610
brunovinicius <85683816+BrunoViniciuszk@users.noreply.github.com>
2025-11-23 14:03:15 -0300

    Polindo

--- src/main/java/com/example/order_api/OrderApiApplication.java
+++ src/main/java/com/example/order_api/OrderApiApplication.java
@@ -2,7 +2,7 @@ package com.example.order_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling; // <--- IMPORTANTE
+import org.springframework.scheduling.annotation.EnableScheduling; 
 
 @SpringBootApplication
 @EnableScheduling
